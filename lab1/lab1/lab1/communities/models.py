497765d92724cc337a4f1e315428db1464ef4775
ST3607 <ST3607@TE.local>
2024-12-02 12:31:31 +0600

    lab2

--- lab1/lab1/lab1/communities/models.py
+++ lab1/lab1/lab1/communities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Communities(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField(max_length=150)
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    free = models.BooleanField()
+
+
+    def __str__(self):
+        return self.title
--- lab1/lab1/lab1/posts/models.py
+++ lab1/lab1/lab1/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
